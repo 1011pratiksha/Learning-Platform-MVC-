726766d970f7da635acacd132d501bd0aed0cc72
mahima <mahimarp2004@gmail.com>
2026-09-02 15:16:30 +0530

    Add User model

---
+++ Learning_platform/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Learning_platform.Models
+{
+    [Table("user")]
+    public class User
+    {
+        [Key]
+        [Column("user_id")]
+        public int UserId { get; set; }
+
+        [Column("user_name")]
+        public string UserName { get; set; } = string.Empty;
+
+        [Column("user_email")]
+        public string UserEmail { get; set; } = string.Empty;
+
+        [Column("user_password")]
+        public string UserPassword { get; set; } = string.Empty;
+   
+}
